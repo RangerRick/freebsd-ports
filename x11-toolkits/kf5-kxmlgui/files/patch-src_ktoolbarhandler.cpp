bd8723044e89798ea71224c4d3ef1a601dfb73be
Zoe Knox <zoe@pixin.net>
2022-02-03 22:54:17 -0500

    Menu shuffling for KDE apps

---
+++ x11-toolkits/kf5-kxmlgui/files/patch-src_ktoolbarhandler.cpp
@@ -0,0 +1,11 @@
+--- src/ktoolbarhandler.cpp.orig	2022-02-04 03:43:04 UTC
++++ src/ktoolbarhandler.cpp
+@@ -30,7 +30,7 @@ const char guiDescription[] =
+     ""
+     "<!DOCTYPE gui><gui name=\"StandardToolBarMenuHandler\">"
+     "<MenuBar>"
+-    "    <Menu name=\"settings\">"
++    "    <Menu name=\"window\">"
+     "        <ActionList name=\"%1\" />"
+     "    </Menu>"
+     "</MenuBar>"
