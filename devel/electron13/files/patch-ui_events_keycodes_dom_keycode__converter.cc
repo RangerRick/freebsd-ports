b3797b3bf65c3a9797cfe803ba5f15d2366b96e8
Zoe Knox <zoe@pixin.net>
2022-06-02 11:09:39 -0400

    Build electron wayland backend; spotifyd with pulseaudio; minor hacks

--- devel/electron13/files/patch-ui_events_keycodes_dom_keycode__converter.cc
+++ devel/electron13/files/patch-ui_events_keycodes_dom_keycode__converter.cc
@@ -1,6 +1,15 @@
---- ui/events/keycodes/dom/keycode_converter.cc.orig	2021-07-15 19:14:08 UTC
-+++ ui/events/keycodes/dom/keycode_converter.cc
-@@ -24,7 +24,7 @@ namespace {
+--- work/electron-13.6.3/src/ui/events/keycodes/dom/keycode_converter.cc.orig	2021-07-15 15:14:08.000000000 -0400
++++ work/electron-13.6.3/src/ui/events/keycodes/dom/keycode_converter.cc	2022-05-30 17:15:46.431115000 -0400
+@@ -13,6 +13,8 @@
+ 
+ #if defined(OS_LINUX) || defined(OS_CHROMEOS)
+ #include <linux/input.h>
++#elif defined(OS_BSD)
++#include <linux/input-event-codes.h>
+ #endif
+ 
+ namespace ui {
+@@ -24,7 +26,7 @@
  #if defined(OS_WIN)
  #define DOM_CODE(usb, evdev, xkb, win, mac, code, id) \
    { usb, win, code }
@@ -9,7 +18,16 @@
  #define DOM_CODE(usb, evdev, xkb, win, mac, code, id) \
    { usb, xkb, code }
  #elif defined(OS_APPLE)
-@@ -148,7 +148,7 @@ int KeycodeConverter::DomCodeToNativeKeycode(DomCode c
+@@ -67,7 +69,7 @@
+ #undef DOM_KEY_MAP
+ #undef DOM_KEY_UNI
+ 
+-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
++#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_BSD)
+ 
+ // The offset between XKB Keycode and evdev code.
+ constexpr int kXkbKeycodeOffset = 8;
+@@ -148,7 +150,7 @@
    return UsbKeycodeToNativeKeycode(static_cast<uint32_t>(code));
  }
  
@@ -18,19 +36,3 @@
  // static
  DomCode KeycodeConverter::XkbKeycodeToDomCode(uint32_t xkb_keycode) {
    // Currently XKB keycode is the native keycode.
-@@ -163,6 +163,7 @@ uint32_t KeycodeConverter::DomCodeToXkbKeycode(DomCode
-   return static_cast<uint32_t>(DomCodeToNativeKeycode(code));
- }
- 
-+#if !defined(OS_BSD)
- // static
- DomCode KeycodeConverter::EvdevCodeToDomCode(int evdev_code) {
-   // Currently XKB keycode is the native keycode.
-@@ -176,6 +177,7 @@ int KeycodeConverter::DomCodeToEvdevCode(DomCode code)
-   // TODO(crbug.com/1135034): Replace with evdev.
-   return XkbKeycodeToEvdevCode(DomCodeToXkbKeycode(code));
- }
-+#endif
- #endif
- 
- // static
